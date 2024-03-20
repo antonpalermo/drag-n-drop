6255c7ea0069e2db76b230ee68ea6c49c0e494f9
antonpalermo <anpbusinessmail@gmail.com>
2024-03-21 00:15:57 +0800

    init pg database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
+import pg from "pg";
 
 async function main() {
   const app = express();
   const port = process.env.PORT || 4545;
 
-  app.get("/", (_, res) => {
+  const db = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
+
+  app.get("/", async (_, res) => {
     return res.status(200).json({ message: "Server OK!" });
+  });
+
+  app.get("/characters", async (req, res) => {
+    let characters = [];
+
+    try {
+      await db.connect();
+      characters = (await db.query("SELECT * FROM characters")).rows;
+      await db.end();
+    } catch (error) {
+      return res.status(500).send("ERROR GET /characters");
+    }
+
+    return res.status(200).json(characters);
   });
 
   app.listen(port, () =>
